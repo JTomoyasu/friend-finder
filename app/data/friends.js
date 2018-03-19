548dc755b7911a9b79b180fbcf269074c0650af0
Joseph Tomoyasu <JTomoyasu@gmail.com>
2018-03-19 10:08:39 -0700

    Update 1

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends=[
+    {
+        "name":"John",
+        "photo":"",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    }
+];
+
+module.exports=friends;
